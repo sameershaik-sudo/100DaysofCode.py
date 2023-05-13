e051ac856f89891d95bab96cca1370658b9f77c7
Sameer Shaik <87945863+sameershaik-sudo@users.noreply.github.com>
2023-05-13 19:43:13 +0530

    Create Day42_Instagram_Follower_Bot.py

---
+++ Day42_Instagram_Follower_Bot.py
@@ -0,0 +1,9 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import ElementClickInterceptedException
+import time
+
+CHROME_DRIVER_PATH = YOUR CHROM DRIVER PATH
+SIMILAR_ACCOUNT = "buzzfeedtasty"
+USERNAME = YOUR INSTAGRAM USERNAME
+PASSWORD = YOUR INSTAGRAM PASSWORD
