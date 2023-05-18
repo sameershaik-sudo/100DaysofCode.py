787752ffacef338d5af0fadc97488e47332491e6
Sameer Shaik <87945863+sameershaik-sudo@users.noreply.github.com>
2023-05-18 22:43:03 +0530

    Update Day43_Data_Entry_Automation.py

--- Day43_Data_Entry_Automation.py
+++ Day43_Data_Entry_Automation.py
@@ -43,3 +43,26 @@ for element in all_price_elements:
     finally:
         all_prices.append(price)
 
+
+# Create Spreadsheet using Google Form
+# Substitute your own path here 👇
+chrome_driver_path = YOUR_PATH_HERE
+driver = webdriver.Chrome(executable_path=chrome_driver_path)
+
+for n in range(len(all_links)):
+    # Substitute your own Google Form URL here 👇
+    driver.get(URL_TO_YOUR_GOOGLE_FORM)
+
+    time.sleep(2)
+    address = driver.find_element_by_xpath(
+        '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[1]/div/div/div[2]/div/div[1]/div/div[1]/input')
+    price = driver.find_element_by_xpath(
+        '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[2]/div/div/div[2]/div/div[1]/div/div[1]/input')
+    link = driver.find_element_by_xpath(
+        '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[3]/div/div/div[2]/div/div[1]/div/div[1]/input')
+    submit_button = driver.find_element_by_xpath('//*[@id="mG61Hd"]/div[2]/div/div[3]/div[1]/div/div')
+
+    address.send_keys(all_addresses[n])
+    price.send_keys(all_prices[n])
+    link.send_keys(all_links[n])
+    submit_button.click()
