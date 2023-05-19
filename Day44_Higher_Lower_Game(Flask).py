b4e7865f118832a5002b5bd1de46bd2510d03290
Sameer Shaik <87945863+sameershaik-sudo@users.noreply.github.com>
2023-05-19 23:32:38 +0530

    Create Day44_Higher_Lower_Game(Flask).py

---
+++ Day44_Higher_Lower_Game(Flask).py
@@ -0,0 +1,19 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+#Decorators to add a tag around text on web page.
+def make_bold(function):
+    def wrapper():
+        return "<b>" + function() + "</b>"
+    return wrapper
+
+def make_emphasis(function):
+    def wrapper():
+        return "<em>" + function() + "</em>"
+    return wrapper
+
+def make_underlined(function):
+    def wrapper():
+        return "<u>" + function() + "</u>"
+    return wrapper
