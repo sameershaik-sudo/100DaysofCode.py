45a6a1b3c93f254f2042b7d907af2a016fd37eef
Sameer Shaik <87945863+sameershaik-sudo@users.noreply.github.com>
2023-05-21 20:36:42 +0530

    Update Day44_Higher_Lower_Game(Flask).py

--- Day44_Higher_Lower_Game(Flask).py
+++ Day44_Higher_Lower_Game(Flask).py
@@ -26,3 +26,21 @@ def hello_world():
            '<img src="https://media.giphy.com/media/hvS1eKlR75hMr0l7VJ/giphy.gif" width=200>'
 
 
+#Different routes using the app.route decorator
+@app.route("/bye")
+@make_bold
+@make_emphasis
+@make_underlined
+def bye():
+    return "Bye!"
+
+
+#Creating variable paths and converting the path to a specified data type
+@app.route("/username/<name>/<int:number>")
+def greet(name, number):
+    return f"Hello there {name}, you are {number} years old!"
+
+
+if __name__ == "__main__":
+    #Run the app in debug mode to auto-reload
+    app.run(debug=True)
