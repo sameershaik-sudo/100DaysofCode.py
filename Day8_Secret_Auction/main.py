70b72bf9febe85f8afbc1f65d169648baaebb767
Sameer Shaik <87945863+sameershaik-sudo@users.noreply.github.com>
2023-07-04 12:16:42 +0530

    Update main.py

--- Day8_Secret_Auction/main.py
+++ Day8_Secret_Auction/main.py
@@ -1,29 +1,29 @@
-from mymodules import clear
+from my_modules import clear
 from art import logo
 print(logo)
 
 bids = {}
+
 bidding_finished = False
 
 def find_highest_bidder(bidding_record):
-  highest_bid = 0
-  winner = ""
-  # bidding_record = {"Angela": 123, "James": 321}
-  for bidder in bidding_record:
-    bid_amount = bidding_record[bidder]
-    if bid_amount > highest_bid: 
-      highest_bid = bid_amount
-      winner = bidder
-  print(f"The winner is {winner} with a bid of ${highest_bid}")
+    highest_bid = 0
+    winner = ""
+    for bidder in bidding_record:
+        bid_amount = bidding_record[bidder]
+        if bid_amount > highest_bid:
+            highest_bid = bid_amount
+            winner = bidder
+    print(f"The Winner is {winner} with a bid of {highest_bid}")
+        
 
 while not bidding_finished:
-  name = input("What is your name?: ")
-  price = int(input("What is your bid?: $"))
-  bids[name] = price
-  should_continue = input("Are there any other bidders? Type 'yes or 'no'.\n")
-  if should_continue == "no":
-    bidding_finished = True
-    find_highest_bidder(bids)
-  elif should_continue == "yes":
-    clear()
-  
+    name = input("What's your name?\n")
+    price = int(input("What's your bid?\n$"))
+    bids[name] = price 
+    should_continue = input("Are there any other bidders? type 'yes' or 'no'.\n")
+    if should_continue == "no":
+        bidding_finished = True
+        find_highest_bidder(bids)
+    elif should_continue == "yes":
+        clear()
