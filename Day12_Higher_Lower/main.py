e79ab49c92ff17920328de39cbf4aad5ba2474e0
Sameer Shaik <87945863+sameershaik-sudo@users.noreply.github.com>
2023-07-12 18:36:00 +0530

    Update main.py

--- Day12_Higher_Lower/main.py
+++ Day12_Higher_Lower/main.py
@@ -1,92 +1,66 @@
+from art import logo, vs
 from game_data import data
 import random
-from art import logo, vs
-from mymodules import clear
+from my_modules import clear
 
-def get_random_account():
-  """Get data from random account"""
-  return random.choice(data)
+def format_data(account):   
+    """Format the account data into a printable format.""" 
+    account_name = account["name"]
+    account_descr = account["description"]
+    account_country = account["country"]
+    return f"{account_name}, a {account_descr}, from {account_country}"
 
-def format_data(account):
-  """Format account into printable format: name, description and country"""
-  name = account["name"]
-  description = account["description"]
-  country = account["country"]
-  # print(f'{name}: {account["follower_count"]}')
-  return f"{name}, a {description}, from {country}"
-
+# use if statement to check if user is correct.
 def check_answer(guess, a_followers, b_followers):
-  """Checks followers against user's guess 
-  and returns True if they got it right.
-  Or False if they got it wrong.""" 
-  if a_followers > b_followers:
-    return guess == "a"
-  else:
-    return guess == "b"
+    """Take the user guess and follower counts and returns if they got it right."""
+    if a_follower_count > b_follower_count:
+        return guess=="a"
+    else:
+        return guess == "b"
 
+# Display art
+print(logo)
 
-def game():
-  print(logo)
-  score = 0
-  game_should_continue = True
-  account_a = get_random_account()
-  account_b = get_random_account()
+score = 0
+game_should_continue = True
+account_b = random.choice(data)
 
-  while game_should_continue:
+# make the game repeatable
+while game_should_continue:
+    
+    # Generate a random account from the game data
+
+    # making the account at position B become the next account at position A
     account_a = account_b
-    account_b = get_random_account()
+    account_b = random.choice(data)
 
-    while account_a == account_b:
-      account_b = get_random_account()
+    while account_a == account_b:           #regenerate another random account if both are equal
+        account_b = random.choice(data)
 
     print(f"Compare A: {format_data(account_a)}.")
     print(vs)
     print(f"Against B: {format_data(account_b)}.")
-    
-    guess = input("Who has more followers? Type 'A' or 'B': ").lower()
+
+    # Ask for the user a guess.
+    guess = input("Who has more followers? Type 'A' or 'B':").lower()
+
+    # Check if user is correct.
+    ## Get follower count of each account.
     a_follower_count = account_a["follower_count"]
     b_follower_count = account_b["follower_count"]
     is_correct = check_answer(guess, a_follower_count, b_follower_count)
 
+    # clear the screen between rounds.
     clear()
     print(logo)
+
+    # give user feedback on their guess.
+    # Score keeping
     if is_correct:
-      score += 1
-      print(f"You're right! Current score: {score}.")
+        score += 1
+        print(f"You're right! Current score: {score}.")
     else:
-      game_should_continue = False
-      print(f"Sorry, that's wrong. Final score: {score}")
-
-game()
-
-'''
-
-FAQ: Why does choice B always become choice A in every round, even when A had more followers? 
-
-Suppose you just started the game and you are comparing the followers of A - Instagram (364k) to B - Selena Gomez (174k). Instagram has more followers, so choice A is correct. However, the subsequent comparison should be between Selena Gomez (the new A) and someone else. The reason is that everything in our list has fewer followers than Instagram. If we were to keep Instagram as part of the comparison (as choice A) then Instagram would stay there for the rest of the game. This would be quite boring. By swapping choice B for A each round, we avoid a situation where the number of followers of choice A keeps going up over the course of the game. Hope that makes sense :-)
-
-'''
+        game_should_continue = False
+        print(f"Sorry, that's wrong. Final score: {score}")
 
 
-
-# Generate a random account from the game data.
-
-# Format account data into printable format.
-
-# Ask user for a guess.
-
-# Check if user is correct.
-## Get follower count.
-## If Statement
-
-# Feedback.
-
-# Score Keeping.
-
-# Make game repeatable.
-
-# Make B become the next A.
-
-# Add art.
-
-# Clear screen between rounds.
