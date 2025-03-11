08e5abc74c823602546c75b1210a5b26da7bf96b
Sameer Shaik <87945863+sameershaik-sudo@users.noreply.github.com>
2025-03-11 20:48:47 +0530

    Update Day17_Turtle_Race.py

--- Day17_Turtle_Race.py
+++ Day17_Turtle_Race.py
@@ -1,38 +1,38 @@
+import turtle
 from turtle import Turtle, Screen
 import random
-
 is_race_on = False
 screen = Screen()
-screen.setup(width=500, height=400)
-user_bet = screen.textinput(title="Make your bet", prompt="Which turtle will win the race? Enter a color: ")
+screen.setup(500,400)
+user_bet = screen.textinput(title="make your bet", prompt="Which turtle will win the race? Enter a color:")
+
 colors = ["red", "orange", "yellow", "green", "blue", "purple"]
-y_positions = [-70, -40, -10, 20, 50, 80]
 all_turtles = []
 
-#Create 6 turtles
-for turtle_index in range(0, 6):
-    new_turtle = Turtle(shape="turtle")
-    new_turtle.penup()
-    new_turtle.color(colors[turtle_index])
-    new_turtle.goto(x=-230, y=y_positions[turtle_index])
-    all_turtles.append(new_turtle)
+def create_turtle(name, i, x):
+    name = Turtle()
+    name.shape("turtle")
+    name.color(colors[i])
+    name.penup()
+    name.goto(x=-230, y=-100+x)
+    all_turtles.append(name)
+
+for i in range(6):
+    create_turtle(colors[i], i, i*30)
 
 if user_bet:
     is_race_on = True
 
 while is_race_on:
     for turtle in all_turtles:
-        #230 is 250 - half the width of the turtle.
-        if turtle.xcor() > 230:
-            is_race_on = False
-            winning_color = turtle.pencolor()
-            if winning_color == user_bet:
-                print(f"You've won! The {winning_color} turtle is the winner!")
-            else:
-                print(f"You've lost! The {winning_color} turtle is the winner!")
-
-        #Make each turtle move a random amount.
-        rand_distance = random.randint(0, 10)
+        rand_distance = random.randint(0,10)
         turtle.forward(rand_distance)
+        if turtle.xcor() > 230: # turtle size is 40x40 , so reduce 20 from finish line
+            is_race_on = False
+            winner = turtle.pencolor()
+            if winner == user_bet:
+                print(f"You won the bet! The {winner} turtle is the winner!")
+            else:
+                print(f"You lost! The winner is the {winner} turtle!")
 
 screen.exitonclick()
